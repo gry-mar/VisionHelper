bb4be0a5e61c513c5de565d84f28f08300efb6d0
NacixWosinska <95026929+NacixWosinska@users.noreply.github.com>
2022-10-16 22:10:58 +0200

    Initial commit

---
+++ app/src/main/java/edu/ib/visionhelper/MainActivity.kt
@@ -0,0 +1,11 @@
+package edu.ib.visionhelper
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
