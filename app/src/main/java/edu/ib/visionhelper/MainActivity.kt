8e9698efb3da2452e974352f2ae9df44d217da53
Martyna Grygiel <254673@student.pwr.edu.pl>
2022-10-18 22:46:27 +0200

    camera screen UI added

--- app/src/main/java/edu/ib/visionhelper/MainActivity.kt
+++ app/src/main/java/edu/ib/visionhelper/MainActivity.kt
@@ -1,11 +1,21 @@
 package edu.ib.visionhelper
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageButton
+import edu.ib.visionhelper.camera.CameraActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val cameraButton = findViewById<ImageButton>(R.id.cameraButton)
+        cameraButton.setOnClickListener{
+            val intent = Intent(this, CameraActivity::class.java)
+            startActivity(intent)
+
+        }
     }
 }
---
+++ app/src/main/java/edu/ib/visionhelper/camera/CameraActivity.kt
@@ -0,0 +1,12 @@
+package edu.ib.visionhelper.camera
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import edu.ib.visionhelper.R
+
+class CameraActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_camera)
+    }
+}
