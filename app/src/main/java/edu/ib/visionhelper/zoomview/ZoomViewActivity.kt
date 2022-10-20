e937785251562f44847ae0571b6c5a7ec6df9150
Martyna Fojcik <254681@student.pwr.edu.pl>
2022-10-20 14:26:47 +0200

    zoomview UI added

---
+++ app/src/main/java/edu/ib/visionhelper/zoomview/ZoomViewActivity.kt
@@ -0,0 +1,12 @@
+package edu.ib.visionhelper.zoomview
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import edu.ib.visionhelper.R
+
+class ZoomViewActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_zoom_view)
+    }
+}
