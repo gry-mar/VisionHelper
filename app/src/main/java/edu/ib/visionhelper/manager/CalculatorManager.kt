4392c7b152985b18d65f10ee4bc04b3f58978c12
NacixWosinska <256283@student.pwr.edu.pl>
2022-10-26 18:39:28 +0200

    calculator math operations

---
+++ app/src/main/java/edu/ib/visionhelper/manager/CalculatorManager.kt
@@ -0,0 +1,30 @@
+package edu.ib.visionhelper.manager
+
+import android.util.Log
+
+class CalculatorManager {
+
+    private var prevNum: Int = 0
+    private var nextNum: Int = 0
+
+    private fun sum(): Int {
+        return prevNum + nextNum
+    }
+
+    private fun subtract(): Int {
+        return prevNum - nextNum
+    }
+
+    private fun divide(): Int {
+        return try {
+            prevNum / nextNum
+        } catch (e: ArithmeticException) {
+            Log.d("ArithmeticFail", "Failed to divide - you cannot divide by zero.")
+        }
+    }
+
+    private fun multiply(): Int {
+        return prevNum * nextNum;
+    }
+
+}
